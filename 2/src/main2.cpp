59909f3dd65e02caf62b38467fe06cc048cf0ae9
Arthur-Kotaro <arthur.kotaro@yandex.ru>
2025-09-08 08:05:23 +0000

    Task1 - OK, Task2 - check

--- 2/src/main2.cpp
+++ 2/src/main2.cpp
@@ -10,9 +10,7 @@ bool CheckNumber(std::string &number)
 {
 	if(number.length() != 10) return false;
 	for(int iter = 0; iter < 10; iter++)
-	{
 		if(number[iter] < '0'|| number[iter] > '9') return false;
-	}
 	return true;
 }
 
@@ -23,9 +21,8 @@ class Contact
 	std::string name;
 	const std::string prefix = "+7";
 	std::string number;
-
 public:
-		Contact()
+	Contact()
 	{
 		std::cout<< "Enter name of the contact: ";
 		std::cin.ignore();
@@ -53,17 +50,29 @@ public:
 class Phone
 {
 	std::vector <Contact*> contactList;
-
 public:
 	Phone()
 	{
-
-
+		if(contactList.size() < 1) std::cout<< "Contact list is empty.\n";
 	}
+	
 	void add()
 	{
-
+		std::string answ;
+		do
+		{
+			std::cout<< "\nAdd new contact? ";
+			std::cin >> answ;
+			if(answ == "y" || answ == "yes" || answ == "YES")
+			{
+				Contact* tmp = new Contact;
+				contactList.push_back(tmp);
+			}
+			else if(answ == "n" || answ == "no" || answ == "NO") break;
+			//else continue;
+		} while(true);
 	}
+
 	std::string	findContact(std::string &contactName)
 	{
 		std::string contactNumber;
@@ -74,7 +83,8 @@ public:
 		}
 		return contactNumber;
 	}
-	void call()
+
+	std::string GetRespondentData()
 	{
 		std::string answ;
 		do
@@ -90,38 +100,51 @@ public:
 				if(answ.length() != 10)
 				{
 					std::cout << "No such contact in the list.\n";
-					return;
+					return std::string();
 				}
+				else return answ;
 			}
 			else if(answ == "num")
 			{
 				std::cin.ignore();
 				std::cout<< "Enter phone number: ";
 				std::getline(std::cin, answ);
-				if(CheckNumber(answ))
-					std::cout<< "Calling to +7" << answ << ".\n";
+				if(CheckNumber(answ)) return answ;
 				else
 				{
 					std::cout<< "Incorrect number. Try again.\n";
-					return;
+					return std::string();
 				}
 			}
-			else
-			{
-				std::cout<< "Incorrect input. Try again.\n";
-			}
+			else std::cout<< "Incorrect input. Try again.\n";
 		} while(true);	
 	}
+
+	void call()
+	{
+		std::string number =  GetRespondentData();
+		if(number.length() != 0)
+			std::cout<< "Calling to +7" << number << ".\n";
+	}
+
 	void sms()
 	{
-
+		std::string number = GetRespondentData();
+		if(number.length() != 0)
+		{
+			std::string MsgTxt; 
+			std::cin.ignore();
+			std::cout<< "Enter text of SMS below.\n";
+			std::getline(std::cin, MsgTxt);
+			std::cout<< "To +7" << number << " sent message:\n" << MsgTxt << std::endl;
+		}
 	}
 };
 
 int main()
 {
     std::string input_str;
-    Phone * user_phone = new Phone;
+    Phone* user_phone = new Phone;
     do
     {
         std::cout<< "\nEnter command (add/call/sms/exit): ";
